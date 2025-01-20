8b4a902f0a7083442debeac927a290c22741fda1
Gian Vieceli <gianvieceli@gmail.com>
2025-01-20 15:13:01 -0300

    exercise e ts end

--- src/index.ts
+++ src/index.ts
@@ -396,3 +396,85 @@ function logPerson(person: Person2) {
 }
 
 persons.forEach(logPerson);
+
+
+// Day 7 - Exercise 2
+
+class Animal {
+    constructor(public name: string) { }
+    move(meters: number): void {
+        console.log(`${this.name} moved ${meters}m.`)
+    }
+}
+
+class Snake extends Animal {
+    override move(meters: number): void {
+        console.log('Slithering...')
+        super.move(meters)
+    }
+}
+
+class Pony extends Animal {
+    override move(meters: number): void {
+        console.log('Galloping...')
+        super.move(meters)
+    }
+}
+
+
+const sammy = new Snake("Sammy the Snake")
+sammy.move(5)
+
+const pokey = new Pony("Pokey the Pony")
+pokey.move(34)
+
+
+// Day 7 - Exercise 3
+
+class Furniture {
+    constructor(protected manufacturer: string = 'IKEA') { } // pode ser usado apenas por subclasses
+}
+
+class Desk extends Furniture {
+    kind() {
+        console.log(`This is a desk made by ${this.manufacturer}`)
+    }
+}
+
+class Chair extends Furniture {
+    kind() {
+        console.log(`This is a chair made by ${this.manufacturer}`)
+    }
+}
+
+const desk = new Desk()
+desk.kind()
+//   desk.manufacturer // Deve retornar um erro
+
+const chair = new Chair()
+chair.kind()
+// chair.manufacturer // Deve retornar um erro
+
+// Day 7 - Exercise 4
+
+let multiply: (val1: number, val2: number) => number;
+let capitalize: (val: string) => string;
+
+capitalize = function (value: string): string {
+    return `${value.charAt(0).toUpperCase()}${value.slice(1)}`;
+}
+
+multiply = function (x: number, y: number): number {
+    return x * y;
+}
+
+console.log(capitalize(`nifty ${multiply(5, 10)}`));
+
+
+// Day 7 - Exercise 5
+
+function layEggs(quantity: number, color: string): void {
+    console.log(`You just laid ${quantity} ${color} eggs. Good job!`);
+}
+
+layEggs(10, "blue");
